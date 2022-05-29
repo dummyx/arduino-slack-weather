06b1345fd8a50b9df75571539bad9ffeea755c7f
dummyx <40601688+dummyx@users.noreply.github.com>
2022-05-30 03:27:33 +0900

    Update

--- daemon.py
+++ daemon.py
@@ -1,44 +1,66 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-
-
-from slack_bolt.app.async_app import AsyncApp
-from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
-
 import os
 import json
+import requests
 import serial
+
 
 ARDUINO_DEVICE_PATH = '/dev/ttyACM0'
 BAUDRATE = 9600
 
-app = AsyncApp(token=os.environ['BOT_TOKEN'])
+BOT_TOKEN = os.environ['BOT_TOKEN']
+USER_TOKEN = os.environ['USER_TOKEN']
+
+BOT_HEADERS = {'Authorization': 'Bearer ' + BOT_TOKEN,
+               'Content-Type': 'application/json; charset=utf-8'}
+
+USER_HEADERS = {'Authorization': 'Bearer ' + USER_TOKEN,
+               'Content-Type': 'application/json; charset=utf-8'}
 
 SLACK_STATUS_API_ENDPOINT = 'https://slack.com/api/users.profile.set'
 SLACK_MESSAGE_API_ENDPOINT = 'https://slack.com/api/chat.postMessage'
 
+SUNNY_EMOJI = ':sunny:'
+RAINY_EMOJI = ':cloud_rain:'
+CLOUDY_EMOJI = ':cloud:'
 
-async def main():
-    # ser = serial.Serial(ARDUINO_DEVICE_PATH, BAUDRATE, timeout=0)
-    # while(ser.isOpen()):
-    #    if (ser.in_waiting > 0):
-    #        data = ser.read(ser.in_waiting)
-    # pass
-    handler = AsyncSocketModeHandler(app, os.environ['WS_TOKEN'])
-    await handler.start_async()
+VALID_INPUT = ['0', '1', '2']
 
-@app.event("app_mention")
-async def event_test(body, say, logger):
-    logger.info(body)
-    await say("What's up?")
+def main():
+    ser = serial.Serial(ARDUINO_DEVICE_PATH, BAUDRATE, timeout=0)
+    oldStatus = 0
+    while True:
+        s = ''
+        if ser.in_waiting > 0:
+            s = ser.read().decode('utf-8')
+            if s in VALID_INPUT:
+                currentStatus = int(s)
+                if currentStatus != oldStatus:
+                    oldStatus = currentStatus
+                    change_status(currentStatus)
+                    send_message(currentStatus)
+            
 
-@app.command("/hello-bolt-python")
-async def command(ack, body, respond):
-    await ack()
-    await respond(f"Hi <@{body['user_id']}>!")
+
+def send_message(s: int):
+    s = "it's raining" if s >= 2 else "now it's sunny" if s <= 0 else "it's cloudy"
+    payload = {
+        "channel": "C03HAES6HFX",
+        "text": s
+    }
+    r = requests.post(SLACK_MESSAGE_API_ENDPOINT, data=json.dumps(payload),
+                      headers=BOT_HEADERS)
+
+
+def change_status(s: int):
+    payload = {
+        'profile': {
+            "status_text": "now in Tokyo",
+            "status_emoji": RAINY_EMOJI if s >= 2 else SUNNY_EMOJI if s <= 0 else CLOUDY_EMOJI,
+            "status_expiration": 0
+        }
+    }
+    r = requests.post(SLACK_STATUS_API_ENDPOINT, data=json.dumps(payload),
+                      headers=USER_HEADERS)
 
 if __name__ == '__main__':
-    import asyncio
-    
-    asyncio.run(main())
+    main()
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -3,45 +3,29 @@
 #define WATER_SENSOR 2
 SI114X si1141;
 
-// isRaining | isCloudy
-bool currentStatus[] = {false, false};
-bool oldStatus[] = {false, false};
+long c = 0;
 
-void setup()
-{
-	Serial.begin(9600);
-  getStatus(oldStatus);
-	pinMode(WATER_SENSOR, INPUT);
+void setup() {
+  Serial.begin(9600);
+  pinMode(WATER_SENSOR, INPUT);
 }
 
-void getStatus(bool status[]) {
-  status[0] = digitalRead(WATER_SENSOR)> 0 ? false : true;
-  status[1] = si1141.ReadVisible() < 256 ? false : true ;
+int getStatus() {
+  return (digitalRead(WATER_SENSOR) ? 0 : 2) + (si1141.ReadVisible() < 256 ? 1 : 0);
 }
 
-bool statusChanged() {
-  return !(oldStatus[0] == currentStatus[0] && oldStatus[1] == currentStatus[1]);
-}
-
-String getOutput() {
-  bool isRaining = currentStatus[0];
-  bool isCloudy = currentStatus[1];
-  if      (isRaining && isCloudy)    return "11";
-  else if (!(isRaining || isCloudy)) return "00";
-  else if (  isRaining )                return "10";
-  else                               return "01";
-}
-
-void copyStatus() {
-  oldStatus[0] = currentStatus[0];
-  oldStatus[1] = currentStatus[1];
-}
-
-void loop()
-{
-	 getStatus(currentStatus);
-   if (statusChanged()) {
-      Serial.println(getOutput());
-   }
-   copyStatus();
+void loop() {
+  if (c > 1000000) {
+    String o;
+    int status = getStatus();
+    switch (status) {
+      case 0: o = "0"; break;
+      case 1: o = "1"; break;
+      case 2: o = "2"; break;
+      case 3: o = "3"; break;
+    }
+    Serial.print(o);
+    c = 0;
+  }
+  c ++;
 }
